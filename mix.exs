052b583d1a6d0b10cb25ef783d0312251ef709e9
slim <dsslimshaddy@gmail.com>
2018-11-14 02:00:26 +0530

    versons

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tokenbalance.MixProject do
     [
       app: :tokenbalance,
       version: "0.1.0",
-      elixir: "~> 1.7.3",
+      elixir: "~> 1.7.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
