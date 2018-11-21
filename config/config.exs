c5d0d30c7a7b959b9b14275a0a46e9b4284b5152
slim <dsslimshaddy@gmail.com>
2018-11-21 20:25:16 +0530

    first commit

--- config/config.exs
+++ config/config.exs
@@ -23,7 +23,7 @@ config :logger, :console,
 config :phoenix, :json_library, Jason
 
 config :ethereumex,
-  url: "https://mainnet.infura.io/v3/2294f3b338ad4524aa9186012810e412"
+  url: "https://rinkeby.infura.io/v3/2294f3b338ad4524aa9186012810e412"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
